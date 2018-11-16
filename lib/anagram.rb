6bfb58aa801b45d51cfac803451e8ec61b2437ad
Miles Bradt <smbradtmichael@gmail.com>
2018-11-16 12:53:52 -0800

    successfully checks for spaces

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,37 +5,45 @@ class String
     check_for_multiple_second = word_one.upcase().split("")
 
     first_has_spaces = check_for_multiple_first.any? { |i|
-      vowels.include? i
+      " ".include? i
     }
 
-    word_one_check = word_one.upcase().split("")
-    word_two_check = word_two.upcase().split("")
-    vowels = ["A", "E", "I", "O", "U"]
-
-    is_vowel_first = word_one_check.any? { |i|
-      vowels.include? i
+    second_has_spaces = check_for_multiple_second.any? { |i|
+      " ".include? i
     }
 
-    is_vowel_second = word_two_check.any? { |i|
-      vowels.include? i
-    }
-
-    is_anagram = word_one_check.all? { |i|
-      word_two_check.include? i
-    }
-
-
-    if (is_vowel_first) | (is_vowel_second)
-      if (is_anagram)
-        p "These words are anagrams"
-        return "These words are anagrams"
-      else
-        p "These words are not anagrams"
-        return "These words are not anagrams"
-      end
+    if (first_has_spaces) | (second_has_spaces)
+      p "These inputs have spaces"
+      return "These inputs have spaces"
     else
-      p "These do not have vowels"
-      return "These do not have vowels"
+      word_one_check = word_one.upcase().split("")
+      word_two_check = word_two.upcase().split("")
+      vowels = ["A", "E", "I", "O", "U"]
+
+      is_vowel_first = word_one_check.any? { |i|
+        vowels.include? i
+      }
+
+      is_vowel_second = word_two_check.any? { |i|
+        vowels.include? i
+      }
+
+      is_anagram = word_one_check.all? { |i|
+        word_two_check.include? i
+      }
+
+      if (is_vowel_first) | (is_vowel_second)
+        if (is_anagram)
+          p "These words are anagrams"
+          return "These words are anagrams"
+        else
+          p "These words are not anagrams"
+          return "These words are not anagrams"
+        end
+      else
+        p "These do not have vowels"
+        return "These do not have vowels"
+      end
     end
   end
 
