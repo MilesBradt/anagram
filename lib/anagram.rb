148716e53a93b847387afdfeaea049d21c58c6f3
Miles Bradt <smbradtmichael@gmail.com>
2018-11-16 10:51:39 -0800

    framework for possible solution, checking two arrays to include same characters

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,14 @@
+class String
+  def include_any?(array)
+    array.any? { |i|
+      self.include? i
+    }
+  end
+end
+
 def anagram(word_one, word_two)
   word_one_check = word_one.split("")
   word_two_check = word_two.split("")
 
-  word_one_check.include?(word_two_check)
+  word_one_check.include_any?(word_two_check)
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,10 +1,12 @@
-require ('rspec')
-require ('pry')
+# Having these on my home PC broke rspec testing
+# require ('rspec')
+# require ('pry')
+
 require ('anagram')
 
-describe(#anagram) do
+describe("#anagram") do
   it("Should check if one word includes the same characters as another word") do
-    expect(anagram("tea", "eat").to(eq(true))
+    expect(anagram("tea", "eat")).to(eq(true))
   end
-  
+
 end
