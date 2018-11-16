4e324bd3aa0c46882fbd2864f05fa95c88753359
Miles Bradt <smbradtmichael@gmail.com>
2018-11-16 11:53:41 -0800

    add capitalization test (not run)

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,10 +4,16 @@
 
 require ('anagram')
 
-describe("#anagram_check?") do
-  new_test = String.new()
-  it("Should check if one word includes the same characters as another word") do
-    expect(new_test.anagram_check?("tea", "eat")).to(eq("These words are anagrams"))
-  end
+  describe("#anagram_check?") do
+    new_test = String.new()
+    it("Should check if one word includes the same characters as another word") do
+      expect(new_test.anagram_check?("tea", "eat")).to(eq("These words are anagrams"))
+    end
+
+    describe("#anagram_check?") do
+      new_test = String.new()
+      it("Should check if two words are anagrams regardless of capitalization") do
+        expect(new_test.anagram_check?("Tea", "Eat")).to(eq("These words are anagrams"))
+      end
 
 end
